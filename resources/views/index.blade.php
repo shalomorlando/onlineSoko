5ef601953a62c0239e56b3191b2c4f85ec51f3bd
edwinwalela <edwinwalela@gmail.com>
2020-06-04 19:40:36 +0300

    home page nav bar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ URL::asset('css/styles.css')}}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css">
+    <title>Online Soko</title>
+</head>
+<body>
+    <nav class="nav-bar">
+        <div>
+            <h1>Online Soko</h1>
+        </div>
+        <div>
+            <ul>
+                <li>Lifestyle</li>
+                <li>Home</li>
+                <li>Wearables</li>
+            </ul>
+        </div>
+        <div>
+            <i class="fas fa-search"></i>
+            <i class="fas fa-user-alt"></i>
+            <i class="fas fa-shopping-bag"></i>
+        </div>
+    </nav>
+    <div class="jumbo">
+        <h1>Online Soko</h1>
+        <p>Premium Non-Essentials</p>
+    </div>
+</body>
+</html>
